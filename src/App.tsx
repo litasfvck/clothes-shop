d7e02a39c4ef489257b4447469062c150f703cc8
Aleksandr Zyrin <alexzyrinjr@gmail.com>
2023-10-16 21:02:10 +0300

    chore: закоментил футер в апп.тсх

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import './scss/app.scss';
-import Home from './pages/Home';
-import Cart from './pages/Cart';
-import Favorite from './pages/Favorite';
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import "./scss/app.scss";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Favorite from "./pages/Favorite";
 
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <Header />
 
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/favorite' element={<Favorite />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/favorite" element={<Favorite />} />
       </Routes>
 
-      <footer>FOOTER</footer>
+      {/* <footer>FOOTER</footer> */}
     </div>
   );
 }
