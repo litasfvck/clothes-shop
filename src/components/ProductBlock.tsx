12ddfdea6b86b2883a00f024e76fc8409d610923
Aleksandr Zyrin <alexzyrinjr@gmail.com>
2023-10-16 19:08:49 +0300

    chore: я фронтендер

--- src/components/ProductBlock.tsx
+++ src/components/ProductBlock.tsx
@@ -1,6 +1,6 @@
-import { FC } from 'react';
-import { IProduct } from '../modals';
-import { MdOutlineFavorite } from 'react-icons/md';
+import { FC } from "react";
+import { IProduct } from "../types";
+import { MdOutlineFavorite } from "react-icons/md";
 
 interface ProductBlockProps {
   product: IProduct;
@@ -8,16 +8,16 @@ interface ProductBlockProps {
 
 const ProductBlock: FC<ProductBlockProps> = ({ product }) => {
   return (
-    <div className='product-container'>
-      <img src={product.image} alt='img' />
-      <div className='info'>
-        <div className='quick-info'>
+    <div className="product-container">
+      <img src={product.image} alt="img" />
+      <div className="info">
+        <div className="quick-info">
           <h4>{product.title}</h4>
           <h5>Price: {product.price} $</h5>
         </div>
 
         <span>
-          <MdOutlineFavorite style={{ width: '25px', height: '25px' }} />
+          <MdOutlineFavorite style={{ width: "25px", height: "25px" }} />
         </span>
       </div>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,24 @@
-import { FC, useEffect, useState } from 'react';
-import Categories from '../components/Categories';
-import ProductBlock from '../components/ProductBlock';
-import axios, { AxiosError } from 'axios';
-import { IProduct } from '../modals';
+import { FC, useEffect, useState } from "react";
+import Categories from "../components/Categories";
+import ProductBlock from "../components/ProductBlock";
+import axios, { AxiosError } from "axios";
+import { IProduct } from "../types";
 
 interface HomeProps {}
 
 const Home: FC<HomeProps> = () => {
   const [products, setProducts] = useState<IProduct[]>([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const fetchProducts = async () => {
     try {
-      setError('');
+      setError("");
       setLoading(true);
-      const response = await axios.get<IProduct[]>(
-        'https://652c1176d0d1df5273ef1c48.mockapi.io/items'
+      const { data } = await axios.get<IProduct[]>(
+        "https://652c1176d0d1df5273ef1c48.mockapi.io/items"
       );
-      setProducts(response.data);
+      setProducts(data);
       setLoading(false);
     } catch (e: unknown) {
       const error = e as AxiosError;
@@ -33,7 +33,7 @@ const Home: FC<HomeProps> = () => {
 
   return (
     <main>
-      <div className='aside-title'>
+      <div className="aside-title">
         <h1>ATSOS</h1>
         <h2>Его designs могут носить люди любого shape, size</h2>
         <h3>Смотря какой fabric</h3>
@@ -44,7 +44,7 @@ const Home: FC<HomeProps> = () => {
       <section>
         {loading && <p>Loading ... </p>}
         {error && <p>{error}</p>}
-        <div className='products-wrapper'>
+        <div className="products-wrapper">
           {products.map((p, index) => (
             <ProductBlock product={p} key={index} />
           ))}
